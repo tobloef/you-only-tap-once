1240b5fab32de7fe889de8aa0563ee2217a7f8a3
Tobias Løfgren <tobloef@gmail.com>
2015-07-14 16:48:47 +0200

    Made the Dot class

--- core/src/com/tobloef/yoto/Dot.java
+++ core/src/com/tobloef/yoto/Dot.java
@@ -1,5 +1,28 @@
 package com.tobloef.yoto;
 
-public class Dot {
+import com.badlogic.gdx.math.Vector3;
 
+public class Dot {
+    private final static int EXPANDING = 1;
+    private final static int IDLE = 0;
+    private final static int SHRINKING = -1;
+
+    public Vector3 position;
+    public Vector3 direction;
+    public float speed;
+    public float size;
+    public boolean actived;
+    public int state;
+    public float lifetime;
+
+    public Dot() {}
+    public Dot(Vector3 position, Vector3 direction, float speed) {
+        this.position = position;
+        this.direction = direction;
+        this.speed = speed;
+        size = 1;
+        actived = false;
+        state = IDLE;
+        lifetime = 0;
+    }
 }
