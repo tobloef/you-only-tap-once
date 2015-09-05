e4319067160e8b32d719e118045f58be0aaa50a6
Tobias Løfgren <tobloef@gmail.com>
2015-09-05 16:40:04 +0200

    Pause menu and icons
    Added some icons and a pause menu.

--- core/src/com/tobloef/yoto/LevelSelectScreen.java
+++ core/src/com/tobloef/yoto/LevelSelectScreen.java
@@ -7,16 +7,17 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane.ScrollPaneStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
-import com.badlogic.gdx.scenes.scene2d.utils.BaseDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class LevelSelectScreen implements Screen {
@@ -24,6 +25,7 @@ public class LevelSelectScreen implements Screen {
     private ShapeRenderer shapeRenderer;
     private BitmapFont menuFont;
     private Texture backButtonTexture;
+    private Texture backButtonTexturePressed;
     private Stage stage;
     private Table table;
     private Table innerTable;
@@ -31,8 +33,10 @@ public class LevelSelectScreen implements Screen {
     private TextButtonStyle buttonStyleOn;
     private TextButtonStyle buttonStyleOff;
     private LabelStyle labelStyle;
+    private ImageButtonStyle backButtonStyle;
     private ScrollPaneStyle scrollPanelStyle;
 
+    private Color red = new Color(200/255f, 45/255f, 25/255f, 1f);
     private Color blue = new Color(60/255f, 145/255f, 215/255f, 1f);
     private Color clear = new Color(60/255f, 145/255f, 215/255f, 0f);
 
@@ -44,7 +48,10 @@ public class LevelSelectScreen implements Screen {
     public void show() {
         shapeRenderer = new ShapeRenderer();
         backButtonTexture = game.manager.get("back_icon.png", Texture.class);
-        menuFont = game.manager.get("menu_font.ttf", BitmapFont.class);
+        backButtonTexturePressed = game.manager.get("back_icon_pressed.png", Texture.class);
+        backButtonTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        backButtonTexturePressed.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        menuFont = game.manager.get("medium_font.ttf", BitmapFont.class);
 
         stage = new Stage(new ScreenViewport()) {
             @Override
@@ -69,13 +76,16 @@ public class LevelSelectScreen implements Screen {
         buttonStyleOff.font = menuFont;
         buttonStyleOff.fontColor = Color.DARK_GRAY;
         
-        
         labelStyle = new LabelStyle();
         labelStyle.font = menuFont;
         labelStyle.fontColor = Color.WHITE;
 
+        backButtonStyle = new ImageButtonStyle();
+        backButtonStyle = new ImageButtonStyle();
+        backButtonStyle.imageUp = new TextureRegionDrawable(new TextureRegion(backButtonTexture));
+        backButtonStyle.imageDown = new TextureRegionDrawable(new TextureRegion(backButtonTexturePressed));
+
         table = new Table();
-        //table.setDebug(true);
         table.setFillParent(true);
         innerTable = new Table();
         for (int i = 0; i < game.levels.size(); i++) {
@@ -105,25 +115,23 @@ public class LevelSelectScreen implements Screen {
             }
         }
         scrollPanelStyle = new ScrollPaneStyle();
-        scrollPanelStyle.vScrollKnob = new BaseDrawable();
         scrollPane = new ScrollPane(innerTable);
         scrollPane.setScrollingDisabled(true, false);
         scrollPane.setupOverscroll(30f, 30f, 150f);
 
-        Image backButton = new Image(backButtonTexture);
-        backButton.setScaling(Scaling.fit);
+        ImageButton backButton = new ImageButton(backButtonStyle);
         backButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new MainMenuScreen(game));
             }
         });
-        table.add(backButton).height(225*game.sizeModifier).pad(20*game.sizeModifier, -10*game.sizeModifier, 0*game.sizeModifier, 0).uniformX().left();
+        table.add(backButton).height(225 * game.sizeModifier).padLeft(game.sizeModifier * 20).padTop(20 * game.sizeModifier).left();
         Label titleLabel = new Label("Levels", labelStyle);
         table.add(titleLabel).uniformX();
         table.add().uniformX();
         table.row();
-        table.add(scrollPane).expandX().colspan(3);
+        table.add(scrollPane).expandX().colspan(3).padTop(game.sizeModifier * -20f);
         stage.addActor(table);
     }
 
@@ -139,13 +147,14 @@ public class LevelSelectScreen implements Screen {
         shapeRenderer.setColor(blue);
         shapeRenderer.rect(0, 0, Gdx.graphics.getWidth(), 10 * game.sizeModifier);
         shapeRenderer.rect(0, 10 * game.sizeModifier, Gdx.graphics.getWidth(), 50 * game.sizeModifier, blue, blue, clear, clear);
-        shapeRenderer.rect(0, Gdx.graphics.getHeight() - 225 * game.sizeModifier, Gdx.graphics.getWidth(), 50 * game.sizeModifier, clear, clear, blue, blue);
+        shapeRenderer.rect(0, Gdx.graphics.getHeight() - 275 * game.sizeModifier, Gdx.graphics.getWidth(), 50 * game.sizeModifier, clear, clear, blue, blue);
         shapeRenderer.end();
         Gdx.gl.glDisable(GL20.GL_BLEND);
     }
 
     @Override
     public void resize(int width, int height) {
+        game.sizeModifier = Math.min(width, height)/1080f;
         show();
     }
 
