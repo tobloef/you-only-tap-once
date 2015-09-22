c4aca3ebd20373bc001d48ca5012f2d99d62a4c1
Tobias Løfgren <tobloef@gmail.com>
2015-09-22 23:12:52 +0200

    Revert "Revert "Added game over screen.""
    This reverts commit 12e6674de72147fb7dfb9b0b60f99199d9c2b48c.

--- desktop/src/com/tobloef/yoto/desktop/DesktopLauncher.java
+++ desktop/src/com/tobloef/yoto/desktop/DesktopLauncher.java
@@ -12,8 +12,13 @@ public class DesktopLauncher {
 		config.title = "You Only Tap Once";
 		config.width = Toolkit.getDefaultToolkit().getScreenSize().width;
 		config.height = Toolkit.getDefaultToolkit().getScreenSize().height;
-		config.width = 700;
-		config.height = 1240;
+		if (true) {
+			config.width = 700;
+			config.height = 1240;
+		} else {
+			config.width = 1240;
+			config.height = 700;
+		}
 		config.fullscreen = false;
 		new LwjglApplication(new YouOnlyTapOnce(), config);
 	}
